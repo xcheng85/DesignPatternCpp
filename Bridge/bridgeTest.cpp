96ed5e27eb48bb277378b9ea03886080390dcc1e
xiao cheng <xcheng4@xiaos-MacBook-Pro.local>
2019-02-24 21:53:18 -0600

    bridge and composite pattern

---
+++ Bridge/bridgeTest.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+struct Renderer
+{
+    virtual string what_to_render_as() const = 0;
+};
+
+struct Shape
+{
+    string name;
+    const Renderer &renderer;
+
+    Shape(const Renderer &renderer) : renderer{renderer}
+    {
+    }
+
+    string str() const
+    {
+        ostringstream oss;
+        oss << "Drawing " << name << " as "
+            << renderer.what_to_render_as();
+        return oss.str();
+    }
+};
+
+struct Triangle : Shape
+{
+    explicit Triangle(const Renderer &renderer)
+        : Shape{renderer}
+    {
+        name = "Triangle";
+    }
+};
+
+struct Square : Shape
+{
+    explicit Square(const Renderer &renderer)
+        : Shape{renderer}
+    {
+        name = "Square";
+    }
+};
+
+struct RasterRenderer : Renderer
+{
+    string what_to_render_as() const override
+    {
+        return "pixels";
+    }
+};
+
+struct VectorRenderer : Renderer
+{
+    string what_to_render_as() const override
+    {
+        return "lines";
+    }
+};
+
+#include "gtest/gtest.h"
+
+namespace
+{
+class Evaluate : public testing::Test
+{
+};
+
+TEST_F(Evaluate, SimpleTest)
+{
+    ASSERT_EQ("Drawing Square as lines", Square{VectorRenderer{}}.str());
+}
+} // namespace
+
+int main(int ac, char *av[])
+{
+    testing::InitGoogleTest(&ac, av);
+    return RUN_ALL_TESTS();
+}
