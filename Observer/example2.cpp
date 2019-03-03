f5cc5c688a0ec12582c19e6439698d9f86854c31
xiao cheng <xcheng4@xiaos-MacBook-Pro.local>
2019-03-03 15:43:34 -0600

    observer pattern

---
+++ Observer/example2.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// independent subject, only coupled with base class: Observer
+class Subject
+{
+    vector<class Observer *> views;
+    int value;
+
+  public:
+    void attach(Observer *obs)
+    {
+        views.push_back(obs);
+    }
+    void setVal(int val)
+    {
+        value = val;
+        notify();
+    }
+    int getVal()
+    {
+        return value;
+    }
+    void notify();
+};
+
+class Observer
+{
+    //dependent functionality
+    Subject *model;
+    int denom;
+
+  public:
+    Observer(Subject *mod, int div)
+    {
+        model = mod;
+        denom = div;
+        //Observers register themselves with the Subject
+        model->attach(this);
+    }
+    virtual void update() = 0;
+
+  protected:
+    Subject *getSubject()
+    {
+        return model;
+    }
+    int getDivisor()
+    {
+        return denom;
+    }
+};
+
+void Subject::notify()
+{
+    //broadcasts
+    for (int i = 0; i < views.size(); i++)
+        views[i]->update();
+}
+
+// View
+class DivObserver : public Observer
+{
+  public:
+    DivObserver(Subject *mod, int div) : Observer(mod, div) {}
+    void update()
+    {
+        // 6. "Pull" information of interest
+        int v = getSubject()->getVal(), d = getDivisor();
+        cout << v << " div " << d << " is " << v / d << '\n';
+    }
+};
+
+class ModObserver : public Observer
+{
+  public:
+    ModObserver(Subject *mod, int div) : Observer(mod, div) {}
+    void update()
+    {
+        int v = getSubject()->getVal(), d = getDivisor();
+        cout << v << " mod " << d << " is " << v % d << '\n';
+    }
+};
+
+int main()
+{
+    Subject subj;
+    DivObserver divObs1(&subj, 4); // 7. Client configures the number and
+    DivObserver divObs2(&subj, 3); //    type of Observers
+    ModObserver modObs3(&subj, 3);
+    subj.setVal(14);
+}
